c9e961ae33a150f205177f1ae07acd2bdf6374de
Elias_Rojas <amzedel7@gmail.com>
2023-05-18 23:52:20 -0400

    dia1

---
+++ datatypes.js
@@ -0,0 +1,5 @@
+// 5 Crea el archivo datatypes.js y utilizar el operador "typeof" de JavaScript para comprobar los diferentes tipos de datos. Comprueba el tipo de datos de cada variable
+   console.log(typeof text);       // "String"
+   console.log(typeof number);     // "Number"
+   console.log(typeof boolean);    // "Boolean"
+   console.log(typeof notDefined); // "undefined"
---
+++ variables.js
@@ -0,0 +1,5 @@
+// 4 Crea un archivo variable.js y declarar variables y asignar tipos de datos string, booleanos, indefinidos y nulos
+let text = "Hola, mi nombre el Elias";
+let number = 7;
+let boolean = true;
+let notDefined;   // undefined
